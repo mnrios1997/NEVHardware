6543427bdc55d829663c5b9bd91a89eb92c5b7c2
Matías Nahuel Rios <mnrios1997@gmail.com>
2021-04-21 20:07:24 -0300

    añado cambios en el carrito

--- cogido.js
+++ cogido.js
@@ -1,7 +1,7 @@
 let selectorCarrito = document.querySelector(".carrito"); //SELECCIONO LA CAJA DEL CARRITO
 let catalogo = document.querySelector(".contenedorCatalogo") //selecciono al catalogo entero
 
-selectorBtnAgregar = catalogo.getElementsByTagName("div"); //getElementsByTagName me duelve todos los div que posee catalogo
+let selectorBtnAgregar = catalogo.getElementsByTagName("div"); //getElementsByTagName me duelve todos los div que posee catalogo
 let producto=[];
 let btnAgregar=[]; 
 for(let i=0;i<selectorBtnAgregar.length;i++){
@@ -15,6 +15,16 @@ for(let i=0;i<btnAgregar.length;i++){
 	//con el for recorro el arreglo de botones y le doy un evento cada boton de cada producto.
 	btnAgregar[i].addEventListener("click",()=>{
 	let crear = document.createElement("DIV");//creo un elemento de tipo div
+	let nombre = document.createElement("H3");
+	let imagencita = document.createElement("IMG");
+	imagencita.classList.add("imagencita");
+	imagencita.src ="Imagenes/Ig.png";
+	
+	console.log(imagencita);
+	crear.appendChild(nombre);
+	crear.appendChild(imagencita);
+	nombre.textContent ="Hola";
+	console.log(nombre);
 	crear.classList.add("verdes"); //al elemento que cree arriba le añado  una clase (verdes) como si lo hiciera en el html. Eso para probar si anda. Pero sirve para ir modificando el css de las cajitas que se van agregando a carrito
 	selectorCarrito.appendChild(crear); //aca solo lo que hago es agregar el div nuevo a la caja carrito
 });
